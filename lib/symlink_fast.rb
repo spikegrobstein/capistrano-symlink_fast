fbb8fdca87e611fbd947804397e79f30ea9bf277
Spike Grobstein <spikegrobstein@mac.com>
2012-05-15 19:37:52 -0400

    added some on :load to capistrano recipe and built init task for symlink_fast

--- lib/symlink_fast.rb
+++ lib/symlink_fast.rb
@@ -2,6 +2,12 @@ require "symlink_fast/version"
 
 module SymlinkFast
 
+  class << self
+    attr_accessor :configs_path
+    attr_accessor :release_path
+    attr_accessor :shared_path
+  end
+
   def self.command( configs )
     configs.map do |config|
      symlink config
--- lib/symlink_fast/capistrano.rb
+++ lib/symlink_fast/capistrano.rb
@@ -2,11 +2,24 @@ require 'capistrano'
 
 Capistrano::Configuration.instance.load do
 
+  on :load do
+    find_and_execute_task('symlink_fast:init')
+  end
+
+  # we want to run the symlinking once the code is copied
+  # but before any code is run that might depend on the
+  # yaml files being in place.
   after 'deploy:copy_code', 'symlink_fast'
 
   namespace symlink_fast do
     task :default, :except => { :no_release => true } do
       run SymlinkFast::command( fetch(:symlink_configs, nil) )
+    end
+
+    task :init do
+      SymlinkFast.release_path = release_path
+      SymlinkFast.shared_path = shared_path
+      SymlinkFast.configs_path = "#{ shared_path }/config"
     end
   end
 
